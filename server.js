3a338cb4b3ba2c48e467aeeb7750234b44bc8c90
n8n-apex <varun@apex-consulting.ai>
2025-08-02 12:03:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,8 @@ app.post('/merge-thumbnail-video', async (req, res) => {
           //await downloadFile(downloadedMusicPath, googleDriveFileIDForMusic);
           await downloadMusicFile(`https://drive.google.com/uc?export=download&id=${thumbnailID}`, downloadedthumbnailPath);
           thumbnailPath = downloadedthumbnailPath;
-          console.log('Music downloaded to:', thumbnailPath);
+          console.log('Thumbnail downloaded to:', thumbnailPath);
+          console.log('VideoPath is:', videoPath);
         } catch (downloadError) {
           console.warn('Failed to download thumbnail:', downloadError.message);
           // Continue without music if download fails
